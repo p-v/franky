bf8348305da4b48d29768f4d75ec979be4f1b693
p-v <pratyushverma@outlook.com>
2015-11-17 15:30:24 +0530

    add few tests

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import unittest
-from parser import parse as p
+from parser import evaluate as p
 
 class TestParser(unittest.TestCase):
     def test_addition(self):
@@ -10,17 +10,16 @@ class TestParser(unittest.TestCase):
         self.assertEqual(p("10/2"),5)
     def test_multiplication(self):
         self.assertEqual(p("3*4"),12)
-
+    def test_division_with_space(self):
+        self.assertEqual(p("10 /2"),5)
+    def test_multiplication_with_space(self):
+        self.assertEqual(p("10 * 2"),20)
     def test_addition_substraction(self):
         self.assertEqual(p("3+5-8"),0)
         self.assertEqual(p("3-8+5"),0)
-
     def test_addition_multiplication(self):
         self.assertEqual(p("5*2-3"),7)
         self.assertEqual(p("-3+5*2"),7)
-    
-    def test_string_with_number(self):
-        self.assertEqual(p("what is 5 +3"),8)
 
 if __name__ == '__main__':
     unittest.main()
